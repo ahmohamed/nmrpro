0c5178dbbed250efceb6d8de677c6592ea9af453
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2015-10-09 01:21:01 +0900

    remove old menu info

--- nmrpro/plugins/FFT/__init__.py
+++ nmrpro/plugins/FFT/__init__.py
@@ -1,23 +1,9 @@
 from .fft import webFFT
+from .fft import *
 from .. import JSinput as inp
-from ..PluginMount import SpecPlugin, JSCommand
+from ..PluginMount import JSCommand
 
-class FFTClass(SpecPlugin):
-    __version__ = '0.1.0'
-    __plugin_name__ = "Fourier transform"
-    __help__ = ""
-
-    interface = {
-    "Processing":{
-        "FFT":{
-            "FFT":{
-                "fun":webFFT,
-                "args":None            
-            },
-        }
-    }
-    }
-
+__all__ = ['fft', 'fft_positive', 'fft_norm']
 
 class FFTCommand(JSCommand):
     menu_path = ['Processing', 'FFT']
