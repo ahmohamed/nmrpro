adfc6b2162b4245f77c59769aade2feb0ede04bf
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2015-12-16 02:20:41 +0900

    release 0.1

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from distutils.core import setup
+from os.path import exists
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
 
 setup(
   name = 'nmrpro',
-  packages = ['nmrpro'], # this must be the same as the name above
+  packages = find_packages(), # this must be the same as the name above
   version = '0.1',
-  description = 'A processing library for processing NMR Spectra',
+  description = 'Interactive processing of NMR Spectra',
   author = 'Ahmed Mohamed',
   author_email = 'mohamed@kuicr.kyoto-u.ac.jp',
   requires=['numpy', 'scipy', 'nmrglue'],
   url = 'https://github.com/ahmohamed/nmrpro', # use the URL to the github repo
-  download_url = 'https://github.com/ahmohamed/nmrpro/tarball/0.1', # I'll explain this in a second
+  #download_url = 'https://github.com/ahmohamed/nmrpro/tarball/0.1',
+  license='MIT',
   keywords = ['nmr', 'spectra', 'multi-dimensional'],
   classifiers = [],
 )
